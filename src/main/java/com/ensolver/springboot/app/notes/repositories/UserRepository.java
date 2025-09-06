27269d047a9df70b5df917ad8292c18c120c7d84
Daniel <hikivel139@gmail.com>
2025-09-06 00:31:54 -0600

    updates

--- src/main/java/com/ensolver/springboot/app/notes/repositories/UserRepository.java
+++ src/main/java/com/ensolver/springboot/app/notes/repositories/UserRepository.java
@@ -3,6 +3,7 @@ package com.ensolver.springboot.app.notes.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ensolver.springboot.app.notes.entity.Usuario;
 
--- src/test/java/com/ensolver/springboot/app/notes/SpringbootApplicationNoteappApplicationTests.java
+++ src/test/java/com/ensolver/springboot/app/notes/SpringbootApplicationNoteappApplicationTests.java
@@ -1,9 +1,15 @@
 package com.ensolver.springboot.app.notes;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.ensolver.springboot.app.notes.repositories.UserRepository;
 
 @SpringBootTest
+
 class SpringbootApplicationNoteappApplicationTests {
 
 	@Test
